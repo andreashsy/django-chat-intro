f2a1a812860bc16b35c862c61903c86050c169e1
unknown <songyanghan+git@gmail.com>
2023-04-30 22:31:47 +0800

    log url

--- mywebsite/asgi.py
+++ mywebsite/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-# from django.core.asgi import get_asgi_application
-from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
+# from channels.routing import get_default_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
@@ -18,8 +18,8 @@ import chat.routing
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mywebsite.settings")
 
 application = ProtocolTypeRouter({
-    # 'http': get_asgi_application(), 
-    'http': get_default_application(), 
+    'http': get_asgi_application(), 
+    # 'http': get_default_application(), 
     'websocket':AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
