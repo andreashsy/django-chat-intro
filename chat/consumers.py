3421b61601b0f237ead49ff97dde290341cfb17f
unknown <songyanghan+git@gmail.com>
2023-05-01 03:18:17 +0800

    add client side disconnect and disconnection messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,12 +6,14 @@ from asgiref.sync import async_to_sync
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = 'test'
+        self.user_id = None
 
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
         self.accept()
+        print(f"Group {self.room_group_name} has {len(self.channel_layer.groups.get(self.room_group_name, {}).items())} connection(s)")
 
 
     def receive(self, text_data=None, bytes_data=None):
@@ -19,6 +21,8 @@ class ChatConsumer(WebsocketConsumer):
         print(f'{str(datetime.now())} - Message received: ', text_data_json)
         message = text_data_json['message']
         user_id = text_data_json['userId']
+        if not self.user_id:
+            self.user_id = user_id
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -38,3 +42,19 @@ class ChatConsumer(WebsocketConsumer):
             'message':message,
             'userId': user_id
         }))
+    
+    def disconnect(self, code=None):
+        print(f'{self.user_id} disconnecting!')
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message': 'Disconnected!',
+                'userId': self.user_id
+            }
+        )
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
