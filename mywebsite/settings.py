916d2cb43ef05f8b9a61647b957fcef303864226
unknown <songyanghan+git@gmail.com>
2023-04-30 21:48:46 +0800

    change host

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -46,7 +46,10 @@ ASGI_APPLICATION = 'mywebsite.asgi.application'
 
 CHANNEL_LAYERS = {
     'default':{
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
+        'BACKEND':'channels.layers.InMemoryChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        }
     }
 }
 
