e8485b1f3b63dcc380f60c68c7edad4148c66514
unknown <songyanghan+git@gmail.com>
2023-05-04 23:44:30 +0800

    add hard coded game logic, works

--- chat/consumers.py
+++ chat/consumers.py
@@ -17,15 +17,32 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        last_msg = cache.get('last_message')
-        if last_msg:
-            print(f"cached last message: " + last_msg)
-        cache.set('last_message', text_data_json['message'], None)
         print(f'{str(datetime.now())} - Message received: ', text_data_json)
+        
+        msg_type = text_data_json['type']
         message = text_data_json['message']
         user_id = text_data_json['userId']
-        if not self.user_id:
+        if not self.user_id and not msg_type == 'server_message':
             self.user_id = user_id
+
+        if msg_type == 'game_move':
+            choice = message
+            game_state = cache.get('game_state')
+            if not game_state:
+                game_state = {self.user_id: choice}
+                cache.set('game_state', game_state)
+                message = "I've made a game choice!"
+            else:
+                print(f'game state: {str(game_state)}') # debug
+                other_player, other_choice = [(k, v) for k, v in game_state.items()][0]
+                is_win = (choice == 'rock' and other_choice == 'scissors') or (choice == 'paper' and other_choice == 'rock') or (choice == 'scissors' and other_choice == 'paper')
+                if other_choice == choice:
+                    message = f"Both {self.user_id} and {other_player} chose {choice}! It's a draw!"
+                elif is_win:
+                    message = f"{self.user_id}'s {choice} wins against {other_player}'s {other_choice}!"
+                else:
+                    message = f"{self.user_id}'s {choice} loses to {other_player}'s {other_choice}!"
+                user_id = 'Server'
 
         await (self.channel_layer.group_send)(
             self.room_group_name,
