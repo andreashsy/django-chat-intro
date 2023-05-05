3d8097179a2f46cc8bf5f692fd4aa64ea9506ff8
unknown <songyanghan+git@gmail.com>
2023-05-06 01:59:28 +0800

    add RPSGameState

--- chat/services/game_service.py
+++ chat/services/game_service.py
@@ -1,7 +1,23 @@
-from models import RPSChoice
+from mywebsite.chat.models.RPSPlayer import RPSPlayer
+from mywebsite.chat.models.RPSGameState import RPSGameState
 
-class RockPaperScissorsGame:
-    def __init__(self, players = None) -> None:
-        self.players = players
+class RPSGameState:
+    def __init__(self) -> None:
+        self.player: RPSPlayer = None
+        self.game_state: RPSGameState = RPSGameState.PREGAME
+
+    def set_player(self, player: RPSPlayer) -> None:
+        self.player = player
+        self.game_state = RPSGameState.WAITING_FOR_OPPONENT
+
+    def reset(self) -> None:
+        self.player = None
+        self.game_state = RPSGameState.PREGAME
+
+    def resolve_against(self, opponent: RPSPlayer) -> RPSGameState:
+        if not self.player: raise ValueError("Cannot resolve if no current player")
+        player = self.player
+        self.reset()
+        return player.determine_result_against(opponent)        
 
     
