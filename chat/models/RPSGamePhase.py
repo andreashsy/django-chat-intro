e11a9649e8944d95b8b2d9d34d28896da4a7a81c
unknown <songyanghan+git@gmail.com>
2023-05-06 02:01:13 +0800

    resolve name conflict

--- chat/models/RPSGamePhase.py
+++ chat/models/RPSGamePhase.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
 
-class RPSGameState(Enum):
+class RPSGamePhase(Enum):
     PREGAME = 'pregame'
     WAITING_FOR_OPPONENT = 'waiting_for_opponent'
--- chat/services/game_service.py
+++ chat/services/game_service.py
@@ -1,20 +1,20 @@
 from mywebsite.chat.models.RPSPlayer import RPSPlayer
-from mywebsite.chat.models.RPSGameState import RPSGameState
+from mywebsite.chat.models.RPSGamePhase import RPSGamePhase
 
 class RPSGameState:
     def __init__(self) -> None:
         self.player: RPSPlayer = None
-        self.game_state: RPSGameState = RPSGameState.PREGAME
+        self.game_state: RPSGamePhase = RPSGamePhase.PREGAME
 
     def set_player(self, player: RPSPlayer) -> None:
         self.player = player
-        self.game_state = RPSGameState.WAITING_FOR_OPPONENT
+        self.game_state = RPSGamePhase.WAITING_FOR_OPPONENT
 
     def reset(self) -> None:
         self.player = None
-        self.game_state = RPSGameState.PREGAME
+        self.game_state = RPSGamePhase.PREGAME
 
-    def resolve_against(self, opponent: RPSPlayer) -> RPSGameState:
+    def resolve_against(self, opponent: RPSPlayer) -> RPSGamePhase:
         if not self.player: raise ValueError("Cannot resolve if no current player")
         player = self.player
         self.reset()
