6a693ddddffd014da1da7bc5379b843dfd0f9c94
unknown <songyanghan+git@gmail.com>
2023-05-06 01:44:29 +0800

    modified RPSPlayer model

--- chat/models.py
+++ chat/models.py
@@ -1,16 +1,21 @@
-from django.db import models
 from enum import Enum
 # Create your models here.
 
 class RPSChoice(Enum):
-    ROCK = 'r'
-    PAPER = 'p'
-    SCISSORS = 's'
+    ROCK = 'rock'
+    PAPER = 'paper'
+    SCISSORS = 'scissors'
+
+class GameResult(Enum):
+    WIN = 'win'
+    DRAW = 'draw'
+    LOSE = 'lose'
 
 class RPSPlayer:
-    def __init__(self, player_id = '', choice = None) -> None:
+    def __init__(self, player_id: str, choice: RPSChoice) -> None:
         self.player_id = player_id
         self.choice = choice
+        self.result: GameResult = None
 
     def set_choice(self, choice: RPSChoice) -> None:
         self.choice = choice
@@ -18,8 +23,18 @@ class RPSPlayer:
     def get_choice(self) -> RPSChoice:
         return self.choice
     
-    def is_win_against(self, opponent) -> bool:
-        return any([(self.choice == RPSChoice.PAPER and opponent.get_choice() == RPSChoice.ROCK),
-                    (self.choice == RPSChoice.SCISSORS and opponent.get_choice() == RPSChoice.PAPER),
-                    (self.choice == RPSChoice.ROCK and opponent.get_choice() == RPSChoice.SCISSORS),
+    def determine_result_against(self, opponent) -> GameResult:
+        result = None
+        is_win = any([(self.choice == RPSChoice.PAPER and opponent.choice == RPSChoice.ROCK),
+                    (self.choice == RPSChoice.SCISSORS and opponent.choice == RPSChoice.PAPER),
+                    (self.choice == RPSChoice.ROCK and opponent.choice == RPSChoice.SCISSORS),
                      ])
+        is_draw = self.choice == opponent.choice
+
+        if is_win:
+            result = GameResult.WIN
+        elif is_draw:
+            result = GameResult.DRAW
+        else:
+            result = GameResult.LOSE
+        return result
